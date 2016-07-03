24839f84ee168537730b9e3e9fdbcf52c0bc9a02
Philip Craig <philipjcraig@gmail.com>
2016-07-03 18:54:28 +1000

    Add benchmark for flat display

--- benches/bench.rs
+++ benches/bench.rs
@@ -18,10 +18,25 @@ fn parse(b: &mut test::Bencher) {
 fn display(b: &mut test::Bencher) {
     let path = std::env::args_os().next().unwrap(); // Note: not constant
     let sections = dwarf::elf::load(path).unwrap();
-    let units: Vec<dwarf::CompilationUnit> = sections.compilation_units().collect().unwrap();
     b.iter(|| {
+        let units: Vec<dwarf::CompilationUnit> = sections.compilation_units().collect().unwrap();
         for unit in &units {
             format!("{}", unit);
+        }
+    });
+}
+
+#[bench]
+fn display_flat(b: &mut test::Bencher) {
+    let path = std::env::args_os().next().unwrap(); // Note: not constant
+    let sections = dwarf::elf::load(path).unwrap();
+    b.iter(|| {
+        let units: Vec<dwarf::CompilationUnitHeader> = sections.compilation_unit_headers().collect().unwrap();
+        for unit in units {
+            let mut entries = unit.entries().unwrap();
+            while let Some(entry) = entries.next().unwrap() {
+                format!("{}", entry);
+            }
         }
     });
 }
